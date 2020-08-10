3faf6f732b7b572dd4ae6fd7577e71b04dfde355
CJ <cjting1128@gmail.com>
2020-08-11 00:22:51 +0800

    Update README

--- chapter-1/1.2/fermat-test.scm
+++ chapter-1/1.2/fermat-test.scm
@@ -1,32 +1,28 @@
-;;Fermat test for prime number
-;; expmod using these two theorems
-;; theorem 1: a^2b % m = (a^b % m)^2 % m
-;; theorem 2: A^(2b + 1) % m = ((a^2b % m) * a) % m
-
-(define (even? n)
-  (= (remainder n 2) 0))
-
-(define (expmod base exp m)
+; Thee reduction steps in the cases where the exponent e is greater than 1
+; are based on the fact that, for any integers x, y, and m,
+; we can find the remainder of x times y modulo m by computing separately
+; the remainders of x modulo m and y modulo m, multiplying these,
+; and then taking the remainder of the result modulo m.
+(define (expomod base exp m)
   (cond
     ((= exp 0) 1)
     ((even? exp)
-      (remainder
-        (square (expmod base (/ exp 2) m))
-        m))
+     (remainder
+       (square (expomod base (/ exp 2) m))
+       m))
     (else
       (remainder
-        (* base (expmod base (- exp 1) m))
+        (* base (expomod base (- exp 1) m))
         m))))
 
 (define (fermat-test n)
   (define (try-it a)
-    (= (expmod a n n) a))
+    (= (expomod a n n) a))
   (try-it (+ 1 (random (- n 1)))))
 
 (define (fast-prime? n times)
-  (cond
-    ((= times 0) true)
-    ((fermat-test n) (fast-prime? n (- times 1)))
-  (else false)))
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
 
-(display (fast-prime? 8 10))
+(display (fast-prime? 9677 10))
