8e08c8b6b30a8201827603bd26c7ffbb01c89ddf
CJ <cjting1128@gmail.com>
2021-01-18 12:34:31 +0800

    To exercise 2.68

--- chapter-2/2.3/2.66.scm
+++ chapter-2/2.3/2.66.scm
@@ -1,14 +1,11 @@
-;; Exercise 2.66
-;; Implement the lookup procedure for the case where the set of records is structured as a binary tree
-
 (define (lookup given-key records)
   (if
-   (null? records) #f
-   (let
-       ((entry-key (key (entry records))))
-     (cond
-      ((= given-key entry-key) (entry records))
-      ((< given-key entry-key)
-       (lookup given-key (left-branch records)))
-      ((> given-key entry-key)
-       (lookup given-key (right-branch records)))))))
+    (null? records) false
+    (let
+      ((entry-key (key (entry records))))
+      (cond
+        ((= given-key entry-key) (entry records))
+        ((< given-key entry-key)
+         (lookup given-key (left-branch records)))
+        ((> given-key entry-key)
+         (lookup given-key (right-branch records)))))))
--- chapter-2/2.3/huffman-tree.scm
+++ chapter-2/2.3/huffman-tree.scm
@@ -1,4 +1,4 @@
-;; Implement Huffman tree
+; Huffman tree implementation
 
 (define (make-leaf symbol weight)
   (list 'leaf symbol weight))
@@ -14,46 +14,50 @@
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
 
+; x may be tree of leaf
 (define (symbols x)
   (if (leaf? x)
-      (list (symbol-leaf tree))
+      (list (symbol-leaf x))
       (caddr x)))
 
+; x may be tree of leaf
 (define (weight x)
   (if (leaf? x)
-      (weight-leaft x)
+      (weight-leaf x)
       (cadddr x)))
 
 (define (decode bits tree)
-  (define (decode-1 bits current-branch)
+  (define (_decode bits current-branch)
     (if (null? bits)
         '()
         (let ((next-branch
-               (choose-branch (car bits) current-branch)))
+                (choose-branch (car bits) current-branch)))
           (if (leaf? next-branch)
               (cons (symbol-leaf next-branch)
-                    (decode-l (cdr bits) tree))
-              (decode-1 (cdr bits) next-branch)))))
-  (decode-1 bits tree))
+                    (_decode (cdr bits) tree))
+              (_decode (cdr bits) next-branch)))))
+  (_decode bits tree))
 
 (define (choose-branch bit current-branch)
   (cond
-   ((= bit 0) (left-branch current-branch))
-   ((= bit 1) (right-branch current-branch))
-   (else (error "bad bit: CHOOSE-BRANCH" bit))))
+    ((= bit 0) (left-branch current-branch))
+    ((= bit 1) (right-branch current-branch))
+    (else (error "bad bit: CHOOSE-BRANCH" bit))))
 
+; set is ordered
 (define (adjoin-set x set)
   (cond
-   ((null? set) (list x))
-   ((< (weight x) (weight (car set)))
-    (cons x set))
-   (else (cons (car set)
-               (adjoin-set x (cdr set))))))
+    ((null? set) (list x))
+    ((< (weight x) (weight (car set)))
+     (cons x set))
+    (else (cons (car set)
+                (adjoin-set x (cdr set))))))
 
+; pairs: a list of symbol-frequency
 (define (make-leaf-set pairs)
   (if (null? pairs)
       '()
       (let ((pair (car pairs)))
         (adjoin-set
-         (make-leaf (car pair) (cadr pair))
-         (make-leaf-set (cdr pairs))))))
+          (make-leaf (car pair) (cadr pair))
+          (make-leaf-set (cdr pairs))))))
